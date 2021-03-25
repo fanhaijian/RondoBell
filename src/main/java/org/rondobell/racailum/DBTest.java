1b9a8a4e42978d84532fddf826340bff9d221dd1
fanhj <10151651@qq.com>
2021-03-25 11:26:34 +0800

    1

---
+++ src/main/java/org/rondobell/racailum/DBTest.java
@@ -0,0 +1,18 @@
+package org.rondobell.racailum;
+
+import org.apache.ibatis.session.SqlSession;
+import org.rondobell.racailum.base.dao.MzMapper;
+import org.rondobell.racailum.base.dao.SqlSessionFactoryHolder;
+
+public class DBTest {
+    public static void main(String[] args) {
+        SqlSession session = SqlSessionFactoryHolder.getSession();
+        MzMapper mapper = session.getMapper(MzMapper.class);
+
+        mapper.simpleUpdate();
+
+        session.close();
+
+
+    }
+}
